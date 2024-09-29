5673424ee425766be447d5176871405b38d31041
Hila <hilarioeylerg@gmail.com>
2024-09-29 16:08:32 -0300

    Agrego derby para simplificar las pruebas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import DTOs.AlumnoDTO;
+import DTOs.AlumnoMapper;
 import entities.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -12,27 +14,74 @@ import services.ServicioInscripcion;
 
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("TP");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
 
-        //Instancias singleton repositorios
-        RepositorioAlumnoImpl repoAlumnos =  FactoryRepositorios.getRepositorioAlumno(em);
-        RepositorioInscripcionImpl repoInscripciones = FactoryRepositorios.getRepositorioInscripcion(em);
-        RepositorioCarreraImpl repoCarreras = FactoryRepositorios.getRepositorioCarrera(em);
+        try {
+            emf = Persistence.createEntityManagerFactory("TP");
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
 
-        ServicioAlumno servicioAlumnos = new ServicioAlumno(repoAlumnos, repoCarreras);
-        ServicioCarrera servicioCarrera = new ServicioCarrera(repoCarreras);
-        ServicioInscripcion servicioInscripcion = new ServicioInscripcion(repoInscripciones);
+            // Setup
+            // Instancias singleton repositorios
+            RepositorioAlumnoImpl repoAlumnos = FactoryRepositorios.getRepositorioAlumno(em);
+            RepositorioInscripcionImpl repoInscripciones = FactoryRepositorios.getRepositorioInscripcion(em);
+            RepositorioCarreraImpl repoCarreras = FactoryRepositorios.getRepositorioCarrera(em);
 
-        System.out.println();
+            //Servicios
+            ServicioAlumno servicioAlumnos = new ServicioAlumno(repoAlumnos, repoCarreras);
+            ServicioCarrera servicioCarrera = new ServicioCarrera(repoCarreras);
+            ServicioInscripcion servicioInscripcion = new ServicioInscripcion(repoInscripciones);
 
-        em.getTransaction().commit();
+            //Alumnos
+            Alumno a1 = new Alumno("Juan", "Gonzalez", 19, "M", "Tandil");
+            Alumno a2 = new Alumno("Maria", "Fernandez", 22, "F", "Mar del Plata");
+            Alumno a3 = new Alumno("Carlos", "Martinez", 18, "M", "Azul");
+            Alumno a4 = new Alumno("Ana", "Lopez", 21, "F", "Laprida");
+            Alumno a5 = new Alumno("Pedro", "Perez", 19, "M", "Tandil");
 
-        em.close();
-        emf.close();
+            //Carreras
+            Carrera c1 = new Carrera("Ingenieria en Sistemas");
+            Carrera c2 = new Carrera("Licenciatura en Gestion Ambiental");
+            Carrera c3 = new Carrera("TUDAI");
+            Carrera c4 = new Carrera("Licenciatura en Fisica");
+            Carrera c5 = new Carrera("Profesorado de Informatica");
+
+            AlumnoMapper alumnoMapper = new AlumnoMapper();
+            AlumnoDTO alumnoDTO1 = alumnoMapper.apply(a1);
+            AlumnoDTO alumnoDTO2 = alumnoMapper.apply(a2);
+            AlumnoDTO alumnoDTO3 = alumnoMapper.apply(a3);
+            AlumnoDTO alumnoDTO4 = alumnoMapper.apply(a4);
+            AlumnoDTO alumnoDTO5 = alumnoMapper.apply(a5);
+
+            servicioAlumnos.altaAlumno(alumnoDTO1);
+            servicioAlumnos.altaAlumno(alumnoDTO2);
+            servicioAlumnos.altaAlumno(alumnoDTO3);
+            servicioAlumnos.altaAlumno(alumnoDTO4);
+            servicioAlumnos.altaAlumno(alumnoDTO5);
+
+            List<AlumnoDTO> alumnosFromDB = servicioAlumnos.listarAlumnos();
+            for(AlumnoDTO alumnoFromDb : alumnosFromDB){
+                System.out.println(alumnoFromDb.getNombre());
+            }
+
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em != null && em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+            if (emf != null) {
+                emf.close();
+            }
+        }
     }
 }
